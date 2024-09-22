0affbe91989ecc005502d996fb982909b4582cb4
jeong chang myung <43950487+jeongcm@users.noreply.github.com>
2024-09-22 23:20:38 +0900

    Update ReactorExample.java

--- src/main/java/com/carrot/reactive/reactor/ReactorExample.java
+++ src/main/java/com/carrot/reactive/reactor/ReactorExample.java
@@ -1,4 +1,14 @@
 package com.carrot.reactive.reactor;
 
-public class ReactorExample {
+public class ReactorExample implements ApplicationRunner {
+
+    public static void main(String[] args) {
+        Flux<String> sequence = Flux.just("HELLO", "JEONG");
+        sequence.map(String::toLowerCase).subscribe(System.out::println);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
 }
