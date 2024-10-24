f063387d3e527c4c01b58c21af652b30db9097ee
jeongcm <jeongcm0101@gmail.com>
2024-10-24 23:57:51 +0900

    main - operator   - 기초 3 and operator 예제

--- src/main/java/com/carrot/reactive/operator/MapExample.java
+++ src/main/java/com/carrot/reactive/operator/MapExample.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.time.Duration;
@@ -28,6 +29,21 @@ public class MapExample implements ApplicationRunner {
         Flux.zip(Flux.just(1, 2, 3).delayElements(Duration.ofMillis(300L)),
                 Flux.just(4, 5, 6).delayElements(Duration.ofMillis(500L))
         ).subscribe(data -> log.info("# onNext: {}", data));
+
+        // and 예제 onComplete 만 전송하게 된다. 데이터는 전달되지 않는다.
+        Mono.just("Task 1")
+                .delayElement(Duration.ofSeconds(1))
+                .doOnNext(data -> log.info("# Mono doOnNext: {}", data))
+                .and(
+                        Flux.just("Task 2", "Task 3")
+                                .delayElements(Duration.ofMillis(300))
+                                .doOnNext(data -> log.info("# Flux doOnNext: {}", data))
+                )
+                .subscribe(
+                        data -> log.info("# onNext: {}", data),
+                        error -> log.error("# onError: {}", error),
+                        () -> log.info("onComplete")
+                );
 
         Thread.sleep(2500L);
     }
